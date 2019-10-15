708f0e1995fea8325bd04dcc38484e8cf35c81d6
matokovacik <12758913+matokovacik@users.noreply.github.com>
2019-10-15 13:26:45 +0200

    Deployment of the sample application

--- main.tf
+++ main.tf
@@ -32,3 +32,23 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     size = "B1"
   }
 }
+
+
+resource "azurerm_app_service" "app_service" {
+  name                = "nc19-appservice"
+  location            = "${azurerm_resource_group.resource_group.location}"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.app_service_plan.id}"
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|cu19registry.azurecr.io/app-dotnet:6"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://cu19registry.azurecr.io"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = "cu19registry"
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = "hWjQZKeQIFSUt9b5vUV/5554MjgX00vW"
+  }
+}
